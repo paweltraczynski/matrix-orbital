911afc9d53a08fc24e60a79185094a9986bc57e4
Pawel Traczynski <pawel.traczynski@gmail.com>
2026-09-24 12:25:13 +0200

    Implemented config file.

---
+++ vfd_info/vfd_info_config.py
@@ -0,0 +1,24 @@
+# Copy this file to vfd_info_config_local.py and do your changes there.
+# Alternatively, you can apply your config changes directly in this file.
+
+wifi_ssid = 'ssid'
+wifi_password = 'password'
+
+# To get the TimeAPI World API key, sign up at:
+# https://timeapi.world/
+# https://rapidapi.com/sleeyax/api/world-time-api3
+# https://rapidapi.com/sleeyax/api/world-time-api3/playground/
+time_api_key = 'your_api_key'
+time_timezone = 'Europe/Warsaw'
+
+# To get the OpenWeatherMap API key, sign up at:
+# https://home.openweathermap.org/
+# https://home.openweathermap.org/api_keys
+weather_api_key = 'your_api_key'
+weather_city = 'Warsaw'
+weather_unit = 'metric'
+
+# Info display night hours diming.
+vfd_dim_hour = 20
+vfd_off_hour = 0
+vfd_on_hour = 8
